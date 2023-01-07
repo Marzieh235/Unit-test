e490fe65c654648e5502d6195fde43ccf1ff3793
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-08 01:15:28 +0330

    Getting to know and start testing with mocha

---
+++ test/first-test.js
@@ -0,0 +1,7 @@
+describe('app' ,function(){
+    it ('my first test' , function(){
+        throw Error ('this is error')
+    })
+} )
+
+
---
+++ test/second-test.js
@@ -0,0 +1,5 @@
+describe('app2' ,function(){
+    it ('my second test' , function(){
+       
+    })
+} )
