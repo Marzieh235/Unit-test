98b1dfe4dabe5a085edaa794a4f3d0d555ca96f3
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-09 15:59:55 +0330

    Test async functions

--- app/libs.js
+++ app/libs.js
@@ -8,16 +8,34 @@ const mul = (x, y) => {
 
 
 const FirstItem = array => {
-    if(array && array.length){
+    if (array && array.length) {
         return array[0];
     }
 }
 
+const delayfilterwithCallback = function (array, filter, callback) {
+    setTimeout(() => {
+        callback(array.filter(filter))
+
+    }, 1000)
+}
+
+
+const delayfilterwithPromise = function(array , filter, callback){
+return new Promise ((resolve , reject)=>{
+    setTimeout(() => {
+        resolve(array.filter(filter))
+
+    }, 1000)
+})
+}
 
 
 module.exports = {
     sum,
     mul,
     FirstItem,
+    delayfilterwithCallback,
+    delayfilterwithPromise
 }
 
--- test/test.js
+++ test/test.js
@@ -1,25 +1,65 @@
 const assert = require('assert');
-const calc = require('./../app/libs');
+const libs = require('./../app/libs');
 
 
 describe('calculator tests', function () {
     it('returns 1 + 31 = 32', function () {
-        assert.strictEqual(calc.sum(1 , 2), 3)
+        assert.strictEqual(libs.sum(1, 2), 3)
     })
 })
 
 
 describe('calculator tests', function () {
     it('returns 2 * 31 = 62', function () {
-        assert.equal(calc.mul(2 , 2), 4)
+        assert.equal(libs.mul(2, 2), 4)
     })
 })
 
 
-describe('first Item' , function(){
-    it('return the first element of an array' , () => {
-        let result = calc.FirstItem([1 , 2 ,3]);
+describe('first Item', function () {
+    it('return the first element of an array', () => {
+        let result = libs.FirstItem([1, 2, 3]);
 
-        assert.equal(result , 1);
+        assert.equal(result, 1);
     })
 })
+
+describe('async test', () => {
+    it('callback : eventually returns the results', (done) => {
+        let input = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let filter = (item) => item % 2 == 0;
+
+
+        libs.delayfilterwithCallback(input, filter, (result) => {
+            assert.deepEqual(result, [2, 4, 6, 8])
+            done();
+        })
+    })
+
+
+
+
+    it('promise : eventually returns the results', () => {
+        let input = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let filter = (item) => item % 2 == 0;
+
+
+        return libs.delayfilterwithPromise(input, filter).then(result => {
+            assert.deepEqual(result, [2, 4, 6, 8]);
+        })
+    })
+
+
+
+    it('async : eventually returns the results', async () => {
+        let input = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let filter = (item) => item % 2 == 0;
+
+        let result = await libs.delayfilterwithPromise(input, filter)
+        assert.deepEqual(result, [2, 4, 6, 8])
+            ;
+
+    })
+
+
+})    
